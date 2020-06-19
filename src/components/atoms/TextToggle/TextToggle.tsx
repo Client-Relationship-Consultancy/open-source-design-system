23901067e71ed4fb15d993bb440a119ef2db5c50
Alex Wong <alex.wong@clientrelationship.com>
2020-06-19 13:59:17 +0100

    stripped a few things away to make it simple to dev. only animating the ball at the moment. made the ball slide left and right on animation

--- src/components/atoms/TextToggle/TextToggle.tsx
+++ src/components/atoms/TextToggle/TextToggle.tsx
@@ -1,8 +1,7 @@
-import React from "react"
-import PropTypes from "prop-types"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
-import { colourPalette } from "../../../brandColours"
+import { colourPalette } from "../../../brandColours";
 
 interface ILabel {
   width: string;
@@ -18,63 +17,81 @@ const Label = styled.label<ILabel>`
   border-radius: 5px;
   opacity: ${props => (props.isDisabled ? "0.4" : "1")};
   cursor: ${props => (props.isDisabled ? "not-allowed" : "pointer")};
-`
+  border-radius: 1.5rem/1.5rem;
+`;
 
-Label.displayName = "Label"
+Label.displayName = "Label";
+
+const SliderContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background-color: ${props => props.theme.black.tint40.hex};
+  transition: 0.2s ease-out;
+`;
+SliderContainer.displayName = "SliderContainer";
 
 const Toggle = styled.input`
+  position: absolute;
   opacity: 0;
   width: 0;
   height: 0;
   :checked + div {
-    left: -100%;
+    background-color: ${props => props.theme.primary.main.hex};
+    > div {
+      left: calc(100% - 1.75rem);
+    }
   }
-`
-Toggle.displayName = "Toggle"
-
-const SliderContainer = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 200%;
-  height: 100%;
-  display: flex;
-  transition: 0.3s ease-out;
-`
-SliderContainer.displayName = "SliderContainer"
+`;
+Toggle.displayName = "Toggle";
 
 const Slider = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
+  width: calc(100% - 1.75rem);
   height: 100%;
-  background-color: ${props => props.theme.black.tint20.hex};
+
   color: ${props => props.theme.black.main.hex};
   font-size: 0.9rem;
-`
+`;
 Slider.defaultProps = {
   theme: colourPalette.examplePalette,
-}
-Slider.displayName = "Slider"
+};
+Slider.displayName = "Slider";
 
 const CheckedSlider = styled(Slider)`
   color: ${props => props.theme.secondary.main.on};
-  background-color: ${props => props.theme.secondary.main.hex};
-`
+`;
 CheckedSlider.defaultProps = {
   theme: colourPalette.examplePalette,
-}
-CheckedSlider.displayName = "CheckedSlider"
+};
+CheckedSlider.displayName = "CheckedSlider";
 
+const BallContainer = styled.div`
+  padding: 5px;
+  height: 1.75rem;
+  width: 1.75rem;
+  border-radius: 50%;
+  position: absolute;
+  transition: 0.4s ease;
+  left: 0;
+`;
+
+const Ball = styled.div`
+  background-color: ${props => props.theme.white.hex};
+  height: 100%;
+  width: 100%;
+  border-radius: 50%;
+`;
 
 interface ITextToggle extends React.HTMLProps<HTMLInputElement> {
   isDisabled: boolean;
   width: string;
   falseOption: React.ReactNode;
   trueOption: React.ReactNode;
-  onBlur: () => void;
-  onChange: () => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
   checked: boolean;
 }
 
@@ -103,22 +120,23 @@ const TextToggle: React.FC<ITextToggle> = (props: ITextToggle) => {
         checked={checked}
         id={id}
         disabled={isDisabled}
-        {...other}
+        // {...other}
       />
       <SliderContainer>
-        <Slider >{falseOption}</Slider>
-        <CheckedSlider>{trueOption}</CheckedSlider>
+        <BallContainer>
+          <Ball />
+        </BallContainer>
       </SliderContainer>
     </Label>
-  )
-}
-TextToggle.displayName = "TextToggle"
+  );
+};
+TextToggle.displayName = "TextToggle";
 
 TextToggle.defaultProps = {
   trueOption: "True",
   falseOption: "False",
   width: "4.5rem",
   isDisabled: false,
-}
+};
 
-export default TextToggle
+export default TextToggle;
