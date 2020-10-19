812d3f75bb7e681d3d5911d7ebe5eb59f58d40ad
Frank Shenton <frank.shenton@clientrelationship.com>
2020-10-19 12:38:24 +0100

    bumped storybook version to 5.3 to ffix bug with addon knobs. Renamed storybook config files as per docs and everything looking good.

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+const path = require("path")
+
+module.exports = {
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs",
+    "@storybook/addon-notes",
+  ],
+}
