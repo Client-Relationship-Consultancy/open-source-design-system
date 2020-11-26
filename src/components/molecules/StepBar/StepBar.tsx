a0eded64132f73b47b492ef916472057f1a7a6cf
Elio <elio.stalteri@clientrelationship.com>
2020-11-26 12:23:55 +0000

    typescripted the component and updated snapshots

--- src/components/molecules/StepBar/StepBar.tsx
+++ src/components/molecules/StepBar/StepBar.tsx
@@ -1,66 +1,77 @@
-import React from "react"
-import styled from "styled-components"
-import PropTypes from "prop-types"
-import Icon from "../../atoms/Icon/Icon"
-import { colourPalette } from "../../../brandColours"
+import React from "react";
+import styled from "styled-components";
+import Icon from "../../atoms/Icon/Icon";
+import { colourPalette } from "../../../brandColours";
 
 const StepWrapper = styled.div`
   display: flex;
   align-items: center;
   margin: 0.5rem 0;
-`
-StepWrapper.displayName = "StepWrapper"
+`;
+StepWrapper.displayName = "StepWrapper";
 
 const StepNumber = styled.div`
   width: 2.5rem;
   height: 2.5rem;
-  border: 3px solid ${props => props.theme.secondary.main.hex};
-  color: ${props => props.theme.secondary.main.hex};
+  border: 3px solid ${(props) => props.theme.secondary.main.hex};
+  color: ${(props) => props.theme.secondary.main.hex};
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 50%;
   font-size: 1.4rem;
   font-weight: bold;
-`
+`;
 StepNumber.defaultProps = {
   theme: colourPalette.examplePalette,
-}
-StepNumber.displayName = "StepNumber"
+};
+StepNumber.displayName = "StepNumber";
 
 const StepName = styled.div`
   margin-left: 0.5rem;
   font-size: 1.1rem;
   &.active {
-    color: ${props => props.theme.secondary.main.hex};
+    color: ${(props) => props.theme.secondary.main.hex};
     font-weight: bold;
   }
-`
+`;
 StepName.defaultProps = {
   theme: colourPalette.examplePalette,
-}
-StepName.displayName = "StepName"
+};
+StepName.displayName = "StepName";
 
 const StepsContainer = styled.div`
   display: flex;
   align-items: center;
   flex-wrap: wrap;
-`
-StepsContainer.displayName = "StepsContainer"
+`;
+StepsContainer.displayName = "StepsContainer";
 
 const Line = styled.hr`
   flex: 1 1 auto;
   margin: 0 1rem;
   border: none;
-  border-top: 2px solid ${props => props.theme.secondary.main.hex};
-`
+  border-top: 2px solid ${(props) => props.theme.secondary.main.hex};
+`;
 Line.defaultProps = {
   theme: colourPalette.examplePalette,
-}
-Line.displayName = "Line"
+};
+Line.displayName = "Line";
 
-class StepBar extends React.PureComponent {
-  renderTotalSteps = (stepNames, currentStep) =>
+interface IStep {
+  order: number;
+  name: string;
+}
+
+interface IStepBar {
+  steps: IStep[];
+  currentStep?: number;
+  id?: string;
+  className?: string;
+}
+
+class StepBar extends React.PureComponent<IStepBar> {
+  renderTotalSteps = (stepNames: IStep[], currentStep: number): React.ReactNode[] =>
     stepNames.map((step, index) => (
       <React.Fragment key={step.order}>
         <StepWrapper className="StepBar__StepWrapper">
@@ -71,27 +82,16 @@ class StepBar extends React.PureComponent {
         </StepWrapper>
         {index < stepNames.length - 1 ? <Line /> : null}
       </React.Fragment>
-    ))
+    ));
 
   render() {
-    const { steps, currentStep, id, className } = this.props
+    const { steps, currentStep, id, className } = this.props;
     return (
       <StepsContainer id={id} className={className}>
-        {this.renderTotalSteps(steps, currentStep)}
+        {this.renderTotalSteps(steps, currentStep || 1)}
       </StepsContainer>
-    )
+    );
   }
 }
 
-StepBar.displayName = "StepBar"
-StepBar.propTypes = {
-  steps: PropTypes.array,
-  currentStep: PropTypes.number,
-  id: PropTypes.string,
-  className: PropTypes.string,
-}
-StepBar.defaultProps = {
-  currentStep: 1,
-}
-
-export default StepBar
+export default StepBar;
