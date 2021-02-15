92a1e26e5d4a2cadbb43ec0bd4d8c1167dcee474
Elio <elio.stalteri@clientrelationship.com>
2021-02-15 16:10:14 +0000

    fixed icon problem

--- src/components/organism/Table/CellRenderers/SelectValueRenderer.js
+++ src/components/organism/Table/CellRenderers/SelectValueRenderer.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 // function to act as a class
 export function SelectValueRenderer() {
 }
@@ -34,9 +35,11 @@ SelectValueRenderer.prototype.renderValue = function() {
 }
 
 SelectValueRenderer.prototype.svg = function() {
-  return "<svg aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"chevron-down\" class=\"svg-inline--fa fa-chevron-down fa-w-14 fa-1x sc-iwsKbI clbLNE\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 448 512\" color=\"black\" shade=\"main\" theme=\"[object Object]\">" +
-    "<path fill=\"currentColor\" d=\"M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z\"></path>" +
+  return (
+    '<style>svg:not(:root).svg-inline--fa {overflow: visible;}.svg-inline--fa.fa-w-14 {width: 0.875em;}.fa-1x {font-size: 1em;}.svg-inline--fa {display: inline-block;font-size: inherit;height: 1em;overflow: visible;vertical-align: -0.125em;}</style><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" class="svg-inline--fa fa-chevron-down fa-w-14 fa-1x sc-iwsKbI clbLNE" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" color="black" shade="main" theme="[object Object]">' +
+    '<path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>' +
     "</svg>"
+  )
 }
 
 SelectValueRenderer.prototype.buttonHtml = function() {
