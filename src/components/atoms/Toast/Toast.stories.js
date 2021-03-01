ea3ae97cd0712dd1d312ca27b349504ff5dcb2ab
Elio <elio.stalteri@clientrelationship.com>
2021-03-01 15:56:05 +0000

    fixed import

--- src/components/atoms/Toast/Toast.stories.js
+++ src/components/atoms/Toast/Toast.stories.js
@@ -2,7 +2,7 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 import { boolean } from "@storybook/addon-knobs"
 import { styled } from "@storybook/theming"
-import { Toast } from "../../../index"
+import { Toast } from "./Toast"
 
 const Container = styled.div`
   position: relative;
